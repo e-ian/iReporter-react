ae903f7a31356d61045b537d150c4168c2049865
e-ian <36891299+e-ian@users.noreply.github.com>
2019-05-28 15:45:01 +0300

    ft(homepage): create homepage that has login and signup buttons (#8)
    - add home page component
    - add tests for component
    - add links to login and signup
    
    [Finishes #166284962]

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,32 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>My iReporter App!</h1>
+        <div className="wrapper">
+          <div className="welcome">
+            <div className="report">
+              <h1>
+                <a href="/" className="logo">
+                  iReporter!
+                </a>
+              </h1>
+            </div>
+
+            <p>
+              <em>Breaking the corruption chain together</em>
+            </p>
+            <p>
+              <em>login to report an incident</em>
+            </p>
+            <p>
+              <a href="login" className="cat-btn">
+                login
+              </a>
+            </p>
+            <h3>
+              <a href="signup">SignUp here</a>
+            </h3>
+          </div>
+        </div>
       </div>
     );
   }
